541054a2b13b1daa146d7e7dd622bee63e507794
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-12-23 20:34:47 +0100

    Add library entity

---
+++ PictureLibrary.Domain/Entities/Library.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace PictureLibrary.Domain.Entities
+{
+    public class Library
+    {
+        public ObjectId Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
