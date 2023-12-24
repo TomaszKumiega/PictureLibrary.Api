585c7e8bfb2d6fefa4a393c83f7055bd0f270e3c
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-12-24 13:18:45 +0100

    Add LibraryController

---
+++ PictureLibrary.Api/Controllers/ControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace PictureLibrary.Api.Controllers
+{
+    public class ControllerBase : Controller
+    {
+        protected string? GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
--- PictureLibrary.Api/Controllers/LibraryController.cs
+++ PictureLibrary.Api/Controllers/LibraryController.cs
@@ -1,6 +1,34 @@
-﻿namespace PictureLibrary.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PictureLibrary.Api.Controllers
 {
-    public class LibraryController
+    [Route("/library")]
+    [ApiController]
+    public class LibraryController : ControllerBase
     {
+        public LibraryController()
+        {
+            
+        }
+
+        [Route("get")]
+        public async Task<IActionResult> Get([FromQuery] string id)
+        {
+            string? userId = GetUserId();
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            if (string.IsNullOrEmpty(id)) 
+            {
+                return BadRequest();
+            }
+
+            //TODO: get library
+
+            return Ok();
+        }
     }
 }
