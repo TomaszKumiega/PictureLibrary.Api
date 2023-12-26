d87faa4c205e74b9e7ed80c4595da46c1cc77fb5
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-12-26 13:14:12 +0100

    Add User entity

---
+++ PictureLibrary.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace PictureLibrary.Domain.Entities
+{
+    public class User : IEntity
+    {
+        public ObjectId Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+    }
+}
