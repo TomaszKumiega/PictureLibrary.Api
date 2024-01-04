f67772b7835ba8d2e30fa7897e9cbf1d086bba71
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-05 00:48:41 +0100

    Add GetAllLibrariesQueryValidator

---
+++ PictureLibrary.Application.Test/Validators/GetAllLibraries/GetAllLibrariesQueryValidatorTest.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation.TestHelper;
+using MongoDB.Bson;
+using PictureLibrary.Application.Query;
+using PictureLibrary.Application.Query.GetAllLibraries;
+
+namespace PictureLibrary.Application.Test.Validators.GetAllLibraries
+{
+    public class GetAllLibrariesQueryValidatorTest
+    {
+        private readonly GetAllLibrariesQueryValidator _validator;
+
+        public GetAllLibrariesQueryValidatorTest()
+        {
+            _validator = new GetAllLibrariesQueryValidator();
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserId_Is_Empty()
+        {
+            var query = new GetAllLibrariesQuery(string.Empty);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Fact]
+        public void Validate_Should_Not_Have_Errors_When_Query_Is_Valid()
+        {
+            var query = new GetAllLibrariesQuery(ObjectId.GenerateNewId().ToString());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserId_Is_Null()
+        {
+            var query = new GetAllLibrariesQuery(null);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserId_Is_Invalid_ObjectId()
+        {
+            var query = new GetAllLibrariesQuery("test");
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+    }
+}
---
+++ PictureLibrary.Application/Query/GetAllLibraries/GetAllLibrariesQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+
+namespace PictureLibrary.Application.Query.GetAllLibraries
+{
+    public class GetAllLibrariesQueryValidator : AbstractValidator<GetAllLibrariesQuery>
+    {
+        public GetAllLibrariesQueryValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must((userId) => ObjectId.TryParse(userId, out _));
+        }
+    }
+}
