e07915c35b3ebcaa7432fad5633d0951c7b30829
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-05 16:15:26 +0100

    Add update tag validators

---
+++ PictureLibrary.Application/Command/UpdateTag/UpdateTagCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+using PictureLibrary.Contracts;
+
+namespace PictureLibrary.Application.Command.UpdateTag
+{
+    public class UpdateTagCommandValidator : AbstractValidator<UpdateTagCommand>
+    {
+        public UpdateTagCommandValidator(AbstractValidator<UpdateTagDto> dtoValidator)
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must(userId => ObjectId.TryParse(userId, out _));
+            RuleFor(x => x.TagId).NotEmpty().Must(tagId => ObjectId.TryParse(tagId, out _));
+            RuleFor(x => x.UpdateTagDto).NotNull().SetValidator(dtoValidator);
+        }
+    }
+}
---
+++ PictureLibrary.Application/DtoValidators/UpdateTagValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PictureLibrary.Contracts;
+using System.Text.RegularExpressions;
+
+namespace PictureLibrary.Application.DtoValidators
+{
+    public partial class UpdateTagValidator : AbstractValidator<UpdateTagDto>
+    {
+        public UpdateTagValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.ColorHex).NotEmpty().Must(x => x != null && HexRegex().IsMatch(x));
+        }
+
+        [GeneratedRegex("^#[0-9a-fA-F]{6}$")]
+        private static partial Regex HexRegex();
+    }
+}
