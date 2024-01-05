143589584da546f09dd725f7d68c0ac1d9ad12ff
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-05 14:31:56 +0100

    Add CreateTagCommand validator

---
+++ PictureLibrary.Application.Test/Fakers/NewTagDtoFaker.cs
@@ -0,0 +1,14 @@
+﻿using AutoBogus;
+using PictureLibrary.Contracts;
+
+namespace PictureLibrary.Application.Test.Fakers
+{
+    public class NewTagDtoFaker : AutoFaker<NewTagDto>
+    {
+        public NewTagDtoFaker()
+        {
+            RuleFor(x => x.Name, f => f.Random.String(5));
+            RuleFor(x => x.ColorHex, x => "#4287f5");
+        }
+    }
+}
---
+++ PictureLibrary.Application.Test/Validators/CreateTag/NewTagValidatorTest.cs
@@ -0,0 +1,100 @@
+﻿using FluentValidation.TestHelper;
+using PictureLibrary.Application.DtoValidators;
+using PictureLibrary.Contracts;
+
+namespace PictureLibrary.Application.Test.Validators.CreateTag
+{
+    public class NewTagValidatorTest
+    {
+        private readonly NewTagValidator _validator;
+
+        public NewTagValidatorTest()
+        {
+            _validator = new NewTagValidator();
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_Name_Is_Null()
+        {
+            var tag = new NewTagDto()
+            {
+                Name = null,
+                ColorHex = "#FFFFFF"
+            };
+
+            var result = _validator.TestValidate(tag);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_Name_Is_Empty()
+        {
+            var tag = new NewTagDto()
+            {
+                Name = "",
+                ColorHex = "#FFFFFF"
+            };
+
+            var result = _validator.TestValidate(tag);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_ColorHex_Is_Null()
+        {
+            var tag = new NewTagDto()
+            {
+                Name = "Tag",
+                ColorHex = null
+            };
+
+            var result = _validator.TestValidate(tag);
+
+            result.ShouldHaveValidationErrorFor(x => x.ColorHex);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_ColorHex_Is_Empty()
+        {
+            var tag = new NewTagDto()
+            {
+                Name = "Tag",
+                ColorHex = ""
+            };
+
+            var result = _validator.TestValidate(tag);
+
+            result.ShouldHaveValidationErrorFor(x => x.ColorHex);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_ColorHex_Is_Not_Color()
+        {
+            var tag = new NewTagDto()
+            {
+                Name = "Tag",
+                ColorHex = "NotColor"
+            };
+
+            var result = _validator.TestValidate(tag);
+
+            result.ShouldHaveValidationErrorFor(x => x.ColorHex);
+        }
+
+        [Fact]
+        public void Validate_Should_Not_Have_Errors_When_Dto_Is_Valid()
+        {
+            var tag = new NewTagDto()
+            {
+                Name = "Tag",
+                ColorHex = "#FFFFFF"
+            };
+
+            var result = _validator.TestValidate(tag);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
