1a6efbd48fdba465af7f3be135bb492e8f443f2c
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-05 01:57:58 +0100

    Add get all tags endpoint

--- PictureLibrary.Api/Controllers/TagsController.cs
+++ PictureLibrary.Api/Controllers/TagsController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using PictureLibrary.Application.Query;
 
 namespace PictureLibrary.Api.Controllers
 {
@@ -10,6 +11,28 @@ namespace PictureLibrary.Api.Controllers
         public TagsController(IMediator mediator) 
             : base(mediator)
         {
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll([FromQuery] string libraryId)
+        {
+            string? userId = GetUserId();
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            if (string.IsNullOrEmpty(libraryId))
+            {
+                return BadRequest();
+            }
+
+            var query = new GetAllTagsQuery(userId, libraryId);
+
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
         }
     }
 }
