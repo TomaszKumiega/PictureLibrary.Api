3f6a8d942541e9162d2fe3284d07743d1bc6f13f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-05 01:42:58 +0100

    Add DeleteLibraryCommandValidator

---
+++ PictureLibrary.Application/Command/DeleteLibrary/DeleteLibraryCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+
+namespace PictureLibrary.Application.Command
+{
+    public class DeleteLibraryCommandValidator : AbstractValidator<DeleteLibraryCommand>
+    {
+        public DeleteLibraryCommandValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must(userId => ObjectId.TryParse(userId, out _));
+            RuleFor(x => x.LibraryId).NotEmpty().Must(libraryId => ObjectId.TryParse(libraryId, out _));
+        }
+    }
+}
