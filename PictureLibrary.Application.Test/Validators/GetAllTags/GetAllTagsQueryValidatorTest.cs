c73e037cdd483d8e56b7a7d446bd5da3703ae4b9
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-05 02:02:11 +0100

    Add GetAllTagsQueryValidator

---
+++ PictureLibrary.Application.Test/Validators/GetAllTags/GetAllTagsQueryValidatorTest.cs
@@ -0,0 +1,86 @@
+﻿using FluentValidation.TestHelper;
+using MongoDB.Bson;
+using PictureLibrary.Application.Query;
+
+namespace PictureLibrary.Application.Test.Validators.GetAllTags
+{
+    public class GetAllTagsQueryValidatorTest
+    {
+        private readonly GetAllTagsQueryValidator _validator;
+
+        public GetAllTagsQueryValidatorTest()
+        {
+            _validator = new GetAllTagsQueryValidator();
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserId_Is_Empty()
+        {
+            var query = new GetAllTagsQuery(string.Empty, ObjectId.GenerateNewId().ToString());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_LibraryId_Is_Empty()
+        {
+            var query = new GetAllTagsQuery(ObjectId.GenerateNewId().ToString(), string.Empty);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.LibraryId);
+        }
+
+        [Fact]
+        public void Validate_Should_Not_Have_Errors_When_Query_Is_Valid()
+        {
+            var query = new GetAllTagsQuery(ObjectId.GenerateNewId().ToString(), ObjectId.GenerateNewId().ToString());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserId_Is_Null()
+        {
+            var query = new GetAllTagsQuery(null, ObjectId.GenerateNewId().ToString());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_LibraryId_Is_Null()
+        {
+            var query = new GetAllTagsQuery(ObjectId.GenerateNewId().ToString(), null);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.LibraryId);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserId_Is_Invalid_ObjectId()
+        {
+            var query = new GetAllTagsQuery("test", ObjectId.GenerateNewId().ToString());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_LibraryId_Is_Invalid_ObjectId()
+        {
+            var query = new GetAllTagsQuery(ObjectId.GenerateNewId().ToString(), "test");
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.LibraryId);
+        }
+    }
+}
---
+++ PictureLibrary.Application/Query/GetAllTags/GetAllTagsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+
+namespace PictureLibrary.Application.Query
+{
+    public class GetAllTagsQueryValidator : AbstractValidator<GetAllTagsQuery>
+    {
+        public GetAllTagsQueryValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must((userId) => ObjectId.TryParse(userId, out _));
+            RuleFor(x => x.LibraryId).NotEmpty().Must((libraryId) => ObjectId.TryParse(libraryId, out _));
+        }
+    }
+}
