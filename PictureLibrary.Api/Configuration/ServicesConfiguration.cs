de3d8661a8246d8d3b8393c5f6efe4b7c3a298ae
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-21 13:49:15 +0100

    Add HashAndSaltService

--- PictureLibrary.Api/Configuration/ServicesConfiguration.cs
+++ PictureLibrary.Api/Configuration/ServicesConfiguration.cs
@@ -4,7 +4,9 @@ using PictureLibrary.Application.Configuration;
 using PictureLibrary.Application.Mapper;
 using PictureLibrary.Domain.Configuration;
 using PictureLibrary.Domain.Repositories;
+using PictureLibrary.Domain.Services;
 using PictureLibrary.Infrastructure.Repositories;
+using PictureLibrary.Infrastructure.Services;
 
 namespace PictureLibrary.Api.Configuration
 {
@@ -32,7 +34,8 @@ namespace PictureLibrary.Api.Configuration
                 .AddMediatR(new MediatRServiceConfiguration().RegisterServicesFromAssembly(typeof(ApplicationEntrypoint).Assembly))
                 .AddValidatorsFromAssembly(typeof(ApplicationEntrypoint).Assembly)
                 .AddSingleton<IMapper, MapperlyMapper>()
-                .AddTransient<IMongoClient, MongoClient>((provider) => new MongoClient(configuration["DbConnectionString"]));
+                .AddTransient<IMongoClient, MongoClient>((provider) => new MongoClient(configuration["DbConnectionString"]))
+                .AddSingleton<IHashAndSaltService, HashAndSaltService>();
         }
     }
 }
---
+++ PictureLibrary.Infrastructure/Services/HashAndSaltService.cs
@@ -0,0 +1,21 @@
+﻿using PictureLibrary.Domain.Services;
+
+namespace PictureLibrary.Infrastructure.Services
+{
+    public class HashAndSaltService : IHashAndSaltService
+    {
+        public HashAndSalt GetHashAndSalt(string text)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(text, nameof(text));
+
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+
+            return new HashAndSalt
+            {
+                Text = text,
+                Hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(text)),
+                Salt = hmac.Key
+            };
+        }
+    }
+}
