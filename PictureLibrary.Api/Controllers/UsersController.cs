42daa187b7b6293d88225491e6a8338766406b48
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-01-21 13:21:07 +0100

    Add UsersController

---
+++ PictureLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PictureLibrary.Api.Controllers
+{
+    [Route("user")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController(IMediator mediator) 
+            : base(mediator)
+        {
+        }
+    }
+}
