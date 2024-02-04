8b9881954fedcee1b923900319e3a679192e684a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-02-04 17:43:02 +0100

    Add update user validators

---
+++ PictureLibrary.Application.Test/Validators/UpdateUser/UpdateUserCommandValidatorTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+using PictureLibrary.Application.Command;
+using PictureLibrary.Application.DtoValidators;
+using PictureLibrary.Application.Test.Fakers;
+
+namespace PictureLibrary.Application.Test.Validators.UpdateUser
+{
+    public class UpdateUserCommandValidatorTest
+    {
+        private readonly UpdateUserCommandValidator _validator;
+
+        public UpdateUserCommandValidatorTest()
+        {
+            _validator = new UpdateUserCommandValidator(new UpdateUserDtoValidator());
+        }
+
+        [Fact]
+        public void Validate_Should_Have_Errors_When_UserDto_Is_Null()
+        {
+            var command = new UpdateUserCommand(null);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserDto);
+        }
+
+        [Fact]
+        public void Validate_Should_Not_Have_Errors_When_UserDto_Is_Not_Null()
+        {
+            var command = new UpdateUserCommand(new UpdateUserDtoFaker().Generate());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.UserDto);
+        }
+    }
+}
---
+++ PictureLibrary.Application/Command/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using PictureLibrary.Contracts;
+
+namespace PictureLibrary.Application.Command
+{
+    public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserCommandValidator(AbstractValidator<UpdateUserDto> dtoValidator)
+        {
+            RuleFor(x => x.UserDto).NotNull().SetValidator(dtoValidator);
+        }
+    }
+}
