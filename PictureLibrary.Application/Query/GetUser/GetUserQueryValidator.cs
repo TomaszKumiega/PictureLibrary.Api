b2f2064e70f6c7e19ba0a4fee0d710c654cd52bb
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-02-04 11:50:29 +0100

    Add GetUserQuery validator

---
+++ PictureLibrary.Application/Query/GetUser/GetUserQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PictureLibrary.Application.Query
+{
+    public class GetUserQueryValidator : AbstractValidator<GetUserQuery>
+    {
+        public GetUserQueryValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty();
+        }
+    }
+}
