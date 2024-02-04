2aa405a2556e8c1a7544e1cffe9ae08b89de3f98
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-02-04 15:00:18 +0100

    Add GetUserHandler

---
+++ PictureLibrary.Application/Query/GetUser/GetUserHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using MongoDB.Bson;
+using PictureLibrary.Application.Mapper;
+using PictureLibrary.Contracts;
+using PictureLibrary.Domain.Entities;
+using PictureLibrary.Domain.Exceptions;
+using PictureLibrary.Domain.Repositories;
+
+namespace PictureLibrary.Application.Query
+{
+    public class GetUserHandler : IRequestHandler<GetUserQuery, UserDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public GetUserHandler(
+            IMapper mapper, 
+            IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            ObjectId userId = ObjectId.Parse(request.UserId);
+            User user = await _userRepository.GetById(userId) ?? throw new NotFoundException();
+        
+            return _mapper.MapToDto(user);
+        }
+    }
+}
