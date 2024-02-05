6241fee944a7b3da60d798e0bbd9c90679952d93
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-02-05 17:31:30 +0100

    Add DeleteUserHandler

---
+++ PictureLibrary.Application/Command/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MongoDB.Bson;
+using PictureLibrary.Domain.Entities;
+using PictureLibrary.Domain.Exceptions;
+using PictureLibrary.Domain.Repositories;
+
+namespace PictureLibrary.Application.Command
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            ObjectId userId = ObjectId.Parse(request.UserId);
+            
+            User user = await _userRepository.GetById(userId) ?? throw new NotFoundException();
+        
+            await _userRepository.Delete(user);
+        }
+    }
+}
