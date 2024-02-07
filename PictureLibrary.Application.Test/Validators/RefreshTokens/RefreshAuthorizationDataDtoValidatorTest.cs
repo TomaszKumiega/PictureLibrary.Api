5f542c6fb9bc167929e1d3a53ea24bc2d2c898a7
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-02-07 18:56:15 +0100

    Add refresh tokens validator

---
+++ PictureLibrary.Application.Test/Validators/RefreshTokens/RefreshAuthorizationDataDtoValidatorTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation.TestHelper;
+using PictureLibrary.Application.DtoValidators;
+using PictureLibrary.Contracts;
+
+namespace PictureLibrary.Application.Test.Validators.RefreshTokens
+{
+    public class RefreshAuthorizationDataDtoValidatorTest
+    {
+        private readonly RefreshAuthorizationDataDtoValidator _validator;
+
+        public RefreshAuthorizationDataDtoValidatorTest()
+        {
+            _validator = new RefreshAuthorizationDataDtoValidator();
+        }
+
+        [Fact]
+        public void Validate_Should_Return_True_When_AccessToken_And_RefreshToken_Are_Not_Empty()
+        {
+            var dto = new RefreshAuthorizationDataDto
+            {
+                AccessToken = "accessToken",
+                RefreshToken = "refresh"
+            };
+
+            var result = _validator.TestValidate(dto);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Validate_Should_Return_False_When_AccessToken_Is_Empty()
+        {
+            var dto = new RefreshAuthorizationDataDto
+            {
+                AccessToken = string.Empty,
+                RefreshToken = "refresh",
+            };
+
+            var result = _validator.TestValidate(dto);
+
+            result.ShouldHaveValidationErrorFor(x => x.AccessToken);
+        }
+
+        [Fact]
+        public void Validate_Should_Return_False_When_RefreshToken_Is_Empty()
+        {
+            var dto = new RefreshAuthorizationDataDto
+            {
+                AccessToken = "accessToken",
+                RefreshToken = string.Empty,
+            };
+
+            var result = _validator.TestValidate(dto);
+
+            result.ShouldHaveValidationErrorFor(x => x.RefreshToken);
+        }
+    }
+}
