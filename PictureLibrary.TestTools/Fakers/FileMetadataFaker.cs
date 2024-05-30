65e5c488aae95d4d3e94ddebcf6dd19c956a24c3
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-05-30 19:04:50 +0200

    Add missing fakers

---
+++ PictureLibrary.TestTools/Fakers/FileMetadataFaker.cs
@@ -0,0 +1,16 @@
+﻿using AutoBogus;
+using MongoDB.Bson;
+using PictureLibrary.Domain.Entities;
+
+namespace PictureLibrary.TestTools.Fakers
+{
+    public class FileMetadataFaker : AutoFaker<FileMetadata>
+    {
+        public FileMetadataFaker()
+        {
+            RuleFor(x => x.Id, x => ObjectId.GenerateNewId());
+            RuleFor(x => x.OwnerId, x => ObjectId.GenerateNewId());
+            RuleFor(x => x.FilePath, x => x.Random.String());
+        }
+    }
+}
---
+++ PictureLibrary.TestTools/Fakers/ImageFileFaker.cs
@@ -0,0 +1,17 @@
+﻿using AutoBogus;
+using MongoDB.Bson;
+using PictureLibrary.Domain.Entities;
+
+namespace PictureLibrary.TestTools.Fakers
+{
+    public class ImageFileFaker : AutoFaker<ImageFile>
+    {
+        public ImageFileFaker()
+        {
+            RuleFor(x => x.Id, x => ObjectId.GenerateNewId());
+            RuleFor(x => x.LibraryId, x => ObjectId.GenerateNewId());
+            RuleFor(x => x.FileId, x => ObjectId.GenerateNewId());
+            RuleFor(x => x.TagIds, x => [ObjectId.GenerateNewId()]);
+        }
+    }
+}
