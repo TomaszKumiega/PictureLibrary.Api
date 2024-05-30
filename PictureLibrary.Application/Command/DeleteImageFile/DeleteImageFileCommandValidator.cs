c562be481306add921aea4d37b196b95e65fa1eb
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-05-30 18:24:15 +0200

    Add DeleteImageFileCommand validator

---
+++ PictureLibrary.Application/Command/DeleteImageFile/DeleteImageFileCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+
+namespace PictureLibrary.Application.Command
+{
+    public class DeleteImageFileCommandValidator : AbstractValidator<DeleteImageFileCommand>
+    {
+        public DeleteImageFileCommandValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must(userId => ObjectId.TryParse(userId, out _));
+            RuleFor(x => x.ImageFileId).NotEmpty().Must(imageFileId => ObjectId.TryParse(imageFileId, out _));
+        }
+    }
+}
