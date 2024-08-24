c75b94387ff746f13eccd4de03c13fe53c412cf5
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-08-24 17:42:46 +0200

    Add GetAllimageFilesQuery validator

---
+++ PictureLibrary.Application/Query/GetAllImageFiles/GetAllImageFilesQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+
+namespace PictureLibrary.Application.Query
+{
+    public class GetAllImageFilesQueryValidator : AbstractValidator<GetAllImageFilesQuery>
+    {
+        public GetAllImageFilesQueryValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must((userId) => ObjectId.TryParse(userId, out _));
+            RuleFor(x => x.LibraryId).NotEmpty().Must((libraryId) => ObjectId.TryParse(libraryId, out _));
+        }
+    }
+}
