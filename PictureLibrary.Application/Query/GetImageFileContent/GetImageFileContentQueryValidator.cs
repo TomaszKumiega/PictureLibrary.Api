67f643a263960e80873871d4163b51d3ed386953
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-08-25 14:05:00 +0200

    Add GetImageFileContentQuery validator

---
+++ PictureLibrary.Application/Query/GetImageFileContent/GetImageFileContentQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MongoDB.Bson;
+
+namespace PictureLibrary.Application.Query
+{
+    public class GetImageFileContentQueryValidator : AbstractValidator<GetImageFileContentQuery>
+    {
+        public GetImageFileContentQueryValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty().Must((userId) => ObjectId.TryParse(userId, out _));
+            RuleFor(x => x.ImageFileId).NotEmpty().Must((imageFileId) => ObjectId.TryParse(imageFileId, out _));
+        }
+    }
+}
