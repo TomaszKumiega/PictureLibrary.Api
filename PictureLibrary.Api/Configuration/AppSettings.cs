7485279469e1d4a392e64c71ffc436e632e1cb73
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-03 15:24:53 +0200

    Fix AppSettings

--- PictureLibrary.Api/Configuration/AppSettings.cs
+++ PictureLibrary.Api/Configuration/AppSettings.cs
@@ -4,6 +4,14 @@ namespace PictureLibrary.Api.Configuration
 {
     public class AppSettings : IAppSettings
     {
+        public AppSettings(IConfiguration configuration)
+        {
+            DatabaseName = configuration["DatabaseName"] ?? string.Empty;
+            TokenPrivateKey = configuration["TokenPrivateKey"] ?? string.Empty;
+            JwtIssuer = configuration["JwtIssuer"] ?? string.Empty;
+            JwtAudience = configuration["JwtAudience"] ?? string.Empty;
+        }
+
         public required string DatabaseName { get; set; }
         public required string TokenPrivateKey { get; set; }
         public required string JwtIssuer { get; set; }
