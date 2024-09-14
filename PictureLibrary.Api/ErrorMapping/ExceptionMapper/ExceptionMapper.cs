29271ccfdf41e819314ad0e6d190e01c489fb39a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-14 19:25:33 +0200

    Add mapping methods

--- PictureLibrary.Api/ErrorMapping/ExceptionMapper/ExceptionMapper.cs
+++ PictureLibrary.Api/ErrorMapping/ExceptionMapper/ExceptionMapper.cs
@@ -9,35 +9,56 @@ namespace PictureLibrary.Api.ErrorMapping
         {
             return exception switch
             {
-                ValidationException e => new ErrorDetails
-                {
-                    StatusCode = 400,
-                    ErrorCode = ErrorCode.ValidationError,
-                    Message = e.Message
-                },
-                InvalidTokenException e => new ErrorDetails
-                {
-                    StatusCode = 401,
-                    ErrorCode = ErrorCode.InvalidToken,
-                    Message = e.Message
-                },
-                NotFoundException e => new ErrorDetails
-                {
-                    StatusCode = 404,
-                    ErrorCode = ErrorCode.NotFound,
-                    Message = e.Message
-                },
-                AlreadyExistsException e => new ErrorDetails
-                {
-                    StatusCode = 409,
-                    ErrorCode = ErrorCode.AlreadyExists,
-                    Message = e.Message
-                },
-                _ => new ErrorDetails
-                {
-                    StatusCode = 500,
-                    Message = "An error occurred"
-                }
+                ValidationException e => GetValidationError(e),
+                InvalidTokenException => GetInvalidTokenError(),
+                NotFoundException => GetNotFoundError(),
+                AlreadyExistsException e => GetAlreadyExistsError(e),
+                _ => GetDefaultError()
+            };
+        }
+
+        private static ErrorDetails GetValidationError(ValidationException e)
+        {
+            return new ErrorDetails
+            {
+                StatusCode = 400,
+                ErrorCode = ErrorCode.ValidationError,
+                Message = e.Message
+            };
+        }
+        private static ErrorDetails GetInvalidTokenError()
+        {
+            return new ErrorDetails
+            {
+                StatusCode = 401,
+                ErrorCode = ErrorCode.InvalidToken,
+                Message = "Invalid token."
+            };
+        }
+        private static ErrorDetails GetNotFoundError()
+        {
+            return new ErrorDetails
+            {
+                StatusCode = 404,
+                ErrorCode = ErrorCode.NotFound,
+                Message = "Resource not found."
+            };
+        }
+        private static ErrorDetails GetAlreadyExistsError(AlreadyExistsException e)
+        {
+            return new ErrorDetails
+            {
+                StatusCode = 409,
+                ErrorCode = ErrorCode.AlreadyExists,
+                Message = e.Message
+            };
+        }
+        private static ErrorDetails GetDefaultError()
+        {
+            return new ErrorDetails
+            {
+                StatusCode = 500,
+                Message = "An error occurred"
             };
         }
     }
