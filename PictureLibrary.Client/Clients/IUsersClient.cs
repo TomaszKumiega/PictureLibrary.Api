a2e716eed4c5df744037197ec5efa8967672e365
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-15 13:32:38 +0200

    Add methods to usersclient interface

--- PictureLibrary.Client/Clients/IUsersClient.cs
+++ PictureLibrary.Client/Clients/IUsersClient.cs
@@ -5,5 +5,9 @@ namespace PictureLibrary.Client.Clients
     public interface IUsersClient
     {
         Task<AuthorizationData> Login(LoginUserRequest request);
+        Task<User> Register(AddUserRequest request);
+        Task<User> GetUser(AuthorizationData authorizationData);
+        Task<User> UpdateUser(UpdateUserRequest request, AuthorizationData authorizationData);
+        Task DeleteUser(AuthorizationData authorizationData);
     }
 }
