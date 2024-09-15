b173e26c1fd4ddba97d559c1cdf698cb3a723075
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-15 13:43:18 +0200

    Add AddLibrary method

--- PictureLibrary.Client/Clients/Libraries/LibrariesClient.cs
+++ PictureLibrary.Client/Clients/Libraries/LibrariesClient.cs
@@ -1,4 +1,5 @@
 ﻿using PictureLibrary.Client.Model;
+using PictureLibrary.Client.Requests;
 
 namespace PictureLibrary.Client.Clients.Libraries
 {
@@ -12,6 +13,11 @@ namespace PictureLibrary.Client.Clients.Libraries
         public async Task<AllLibraries> GetAllLibraries()
         {
             return await apiHttpClient.Get<AllLibraries>("library/getall");
+        }
+
+        public async Task<Library> AddLibrary(AddLibraryRequest request, AuthorizationData authorizationData)
+        {
+            return await apiHttpClient.Post<Library>("library/create", request, authorizationData);
         }
     }
 }
---
+++ PictureLibrary.Client/Requests/AddLibraryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PictureLibrary.Client.Requests
+{
+    public class AddLibraryRequest
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
