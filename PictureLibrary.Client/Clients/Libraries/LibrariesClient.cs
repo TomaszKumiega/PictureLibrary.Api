c518e91dc3e7d655adb011e42df206d79f7b7926
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-15 13:37:31 +0200

    Add GetLibrary method

--- PictureLibrary.Client/Clients/Libraries/LibrariesClient.cs
+++ PictureLibrary.Client/Clients/Libraries/LibrariesClient.cs
@@ -1,6 +1,12 @@
-﻿namespace PictureLibrary.Client.Clients.Libraries
+﻿using PictureLibrary.Client.Model;
+
+namespace PictureLibrary.Client.Clients.Libraries
 {
-    public class LibrariesClient
+    public class LibrariesClient(IApiHttpClient apiHttpClient)
     {
+        public async Task<Library> GetLibrary(string id)
+        {
+            return await apiHttpClient.Get<Library>($"library/get/{id}");
+        }
     }
 }
---
+++ PictureLibrary.Client/Model/Library.cs
@@ -0,0 +1,9 @@
+﻿namespace PictureLibrary.Client.Model
+{
+    public class Library
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
