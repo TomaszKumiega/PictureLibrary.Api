33a4c410d6478a54ebc6eed6118ab7242986c59f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-15 18:45:55 +0200

    Add AddTag method

--- PictureLibrary.Client/Clients/Tags/TagsClient.cs
+++ PictureLibrary.Client/Clients/Tags/TagsClient.cs
@@ -1,4 +1,5 @@
 ﻿using PictureLibrary.Client.Model;
+using PictureLibrary.Client.Requests;
 
 namespace PictureLibrary.Client.Clients.Tags
 {
@@ -7,6 +8,11 @@ namespace PictureLibrary.Client.Clients.Tags
         public async Task<AllTags> GetAllTags(string libraryId, AuthorizationData authorizationData)
         {
             return await client.Get<AllTags>($"tags/getall?libraryId={libraryId}", authorizationData);
+        }
+
+        public async Task<Tag> AddTag(string libraryId, AddTagRequest request, AuthorizationData authorizationData)
+        {
+            return await client.Post<Tag>($"tags/add?libraryId={libraryId}", request, authorizationData);
         }
     }
 }
---
+++ PictureLibrary.Client/Requests/AddTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PictureLibrary.Client.Requests
+{
+    public class AddTagRequest
+    {
+        public required string Name { get; set; }
+        public required string ColorHex { get; set; }
+    }
+}
