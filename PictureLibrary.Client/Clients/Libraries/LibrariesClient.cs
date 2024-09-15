acba2468fc23e6aa3709b835bc56b6c578e2e49f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-09-15 13:47:30 +0200

    Add delete library method

--- PictureLibrary.Client/Clients/Libraries/LibrariesClient.cs
+++ PictureLibrary.Client/Clients/Libraries/LibrariesClient.cs
@@ -7,7 +7,7 @@ namespace PictureLibrary.Client.Clients.Libraries
     {
         public async Task<Library> GetLibrary(string id, AuthorizationData authorizationData)
         {
-            return await apiHttpClient.Get<Library>($"library/get/?id={id}", authorizationData);
+            return await apiHttpClient.Get<Library>($"library/get?id={id}", authorizationData);
         }
 
         public async Task<AllLibraries> GetAllLibraries(AuthorizationData authorizationData)
@@ -23,6 +23,11 @@ namespace PictureLibrary.Client.Clients.Libraries
         public async Task<Library> UpdateLibrary(string libraryId, UpdateLibraryRequest request, AuthorizationData authorizationData)
         {
             return await apiHttpClient.Post<Library>($"library/update?libraryId={libraryId}", request, authorizationData);
+        }
+
+        public async Task DeleteLibrary(string id, AuthorizationData authorizationData)
+        {
+            await apiHttpClient.Get<Library>($"library/delete?id={id}", authorizationData);
         }
     }
 }
